916dd8b6d109713481cdaf3e84bf9c600427bd85
Sterling <39170397+sterlingfire@users.noreply.github.com>
2021-04-23 10:12:35 -0700

    adding default props for flight

--- pages/Flight/Flight.jsx
+++ pages/Flight/Flight.jsx
@@ -43,4 +43,40 @@ const Flight = ({itinerary, departureLeg, returnLeg}) => (
   </Card>
 );
 
+Flight.defaultProps = {
+  // {itinerary, departureLeg, returnLeg}
+  itinerary:{
+    "id": "it_1",
+    "legs": [
+      "leg_1",
+      "leg_4"
+    ],
+    "price": "£35",
+    "agent": "Wizzair.com",
+    "agent_rating": 9.1
+  },
+  departureLeg: {
+    "id": "leg_1",
+    "departure_airport": "BUD",
+    "arrival_airport": "LTN",
+    "departure_time": "2020-10-31T15:35",
+    "arrival_time": "2020-10-31T17:00",
+    "stops": 0,
+    "airline_name": "Wizz Air",
+    "airline_id": "WZ",
+    "duration_mins": 145
+  },
+  returnLeg: {
+    "id": "leg_4",
+    "departure_airport": "LTN",
+    "arrival_airport": "BUD",
+    "departure_time": "2020-11-11T19:45",
+    "arrival_time": "2020-11-11T21:10",
+    "stops": 0,
+    "airline_name": "Wizz Air",
+    "airline_id": "WZ",
+    "duration_mins": 145
+  }
+}
+
 export default Flight;
